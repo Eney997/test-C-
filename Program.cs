4847f7ec3e859f283bf3629310f2a9217ea2b097
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-11-01 15:20:37 +0400

    sCommit

--- Program.cs
+++ Program.cs
@@ -200,16 +200,260 @@ namespace learnCSHARP
 
 
 
-            //10)METHOS
 
-            sayHello("rene",24);
+
+
+
+
+
+            /* //10)METHOS
+             //1)method
+             sayHello("rene",24);
+             Console.ReadLine();
+            */
+
+
+
+
+
+
+
+
+
+            //11)RETURN STATEMANT
+            /*
+            int cubed = cube(2);
+            Console.WriteLine(cubed);
+
+            Console.ReadLine();
+            */
+
+
+
+
+
+
+
+
+
+            //11)IF STATEMANT
+            /*
+            bool isMale = true;
+            bool isTall = false;
+
+            if(isMale && isTall)
+            {
+                Console.WriteLine("You are male and Tall");
+            }
+            else if(isMale && !isTall)
+            {
+                Console.WriteLine("You are male but not tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and you are not tall");
+            }
+
             Console.ReadLine();
 
+            */
+
+
+
+
+
+
+
+            //12)IF STATEMANT (CON'T)
+            /*
+            Console.WriteLine(GetMax(212, 125,19));
+
+            Console.ReadLine();
+            */
+
+
+
+
+
+
+
+
+            //13)UPGRADED CALCULATOR
+            /*
+            double num1, num2;
+            string op;
+
+            Console.Write("Enter your first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your operator: ");
+            op = Console.ReadLine();
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if(op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if(op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if(op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input");
+            }
+
+            Console.ReadLine();
+            
+             */
+
+
+
+
+
+
+
+
+
+            //14)SWITCH STATEMANT
+            /*
+            Console.WriteLine(GetDay(3));
+            Console.ReadLine();
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+            //15)FOR, WHILE ,DO WHILE LOOPS
+            /*int i = 0;
+            while(i<=5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+
+            for(int j = 0; j<=5 ; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            } while (k <= 5);
+           
+
+
+            Console.ReadLine();
+            */
+
+
+
+
+
+
+            //16)GUESSING GAME
+
+
+
+
+
+
         }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "orshabati";
+                    break;
+
+                case 1:
+                    dayName = "samshabati";
+                    break;
+
+                case 2:
+                    dayName = "otxshabati";
+                    break;
+
+                case 3:
+                    dayName = "xutshabati";
+                    break;
+                
+                case 4:
+                    dayName = "paraskevi";
+                    break;
+
+                case 5:
+                    dayName = "shabati";
+                    break;
+
+                case 6:
+                    dayName = "kvira";
+                    break;
+
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+
+            return dayName;
+        }
+
+        static int GetMax(int num1,int num2,int num3)
+        {
+            int result;
+
+            if (num1 > num2 && num1 > num3)
+            {
+                result = num1;
+            }
+            else if(num2 > num1 && num2 > num3)
+            {
+                result = num2;
+            }
+            else
+            {
+               result = num3;
+            }
+            return result;
+        }
+
         static void sayHello(string name,int age)
         {
             Console.WriteLine("Hello bitch " + name + " You are "+ age + " years old");
         }
+
+        static int cube(int number)
+        {
+            int resulet = number * number * number;
+            return resulet;
+        }
+
 
     }
 }
